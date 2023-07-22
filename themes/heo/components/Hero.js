6d78239fece79acf741cd6b38964f5862f61344e
你好呀 <111191349+88lin@users.noreply.github.com>
2023-07-22 22:43:21 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -228,7 +228,7 @@ function TodayCard({ cRef }) {
                     <div id='more' className='select-none'>更多推荐</div>
                 </div>
             </div>
-            <div id='today-card-cover' className={`${isCoverUp ? '' : ' pointer-events-none'} cursor-pointer today-card-cover absolute w-full h-full top-0`} style={{ background: "url('https://bu.dusays.com/2023/03/12/640dcd3a1b146.png') no-repeat center /cover" }}></div>
+            <div id='today-card-cover' className={`${isCoverUp ? '' : ' pointer-events-none'} cursor-pointer today-card-cover absolute w-full h-full top-0`} style={{ background: "url('https://jsd.cdn.zzko.cn/gh/88lin/PicX@master/20230102/banner-sticker.6vj0bovorjc0.webp') no-repeat center /cover" }}></div>
         </div>
     </div>
 }
