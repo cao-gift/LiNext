6a6c6c3c7b0e61a521116f8b96e9db3a7db4e431
你好呀 <111191349+88lin@users.noreply.github.com>
2023-07-25 17:22:09 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -41,8 +41,8 @@ const BLOG = {
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css'
     'https://fonts.googleapis.com/css?family=Bitter&display=swap',// Bitter 用作英文数字的字体
-    'https://fonts.googleapis.com/css?family=Helvetica:wght@300&display=swap',
-    'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css'
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
