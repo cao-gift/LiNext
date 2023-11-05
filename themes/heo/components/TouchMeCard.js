7de4f2b4cebf5bd37c78543332a0f0919b73775c
故人 <111191349+88lin@users.noreply.github.com>
2023-11-05 23:11:38 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -15,9 +15,9 @@ export default function TouchMeCard() {
                 className='cursor-pointer lg:p-6 p-4 border rounded-xl bg-[#4f65f0] dark:bg-yellow-600 dark:border-gray-600'
                 frontContent={
                     <div className='h-full'>
-                        <h2 className='font-[1000] text-3xl'>交流频道</h2>
-                        <h3 className='pt-2'>加入我们的社群讨论分享</h3>
-                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://bu.dusays.com/2023/05/16/64633c4cd36a9.png) center center no-repeat' }}></div>
+                        <h2 className='font-[1000] text-3xl'></h2>
+                        <h3 className='pt-2'></h3>
+                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://img02.anheyu.com/adminuploads/1/2022/09/11/631ddb7c9b250.png) center center no-repeat' }}></div>
                     </div>}
                 backContent={<Link href={CONFIG.SOCIAL_CARD_URL}>
                     <div className='font-[1000] text-xl h-full'>
