9e100213899f843c3c35765a09c8f49a3c8a3998
故人 <111191349+88lin@users.noreply.github.com>
2023-11-08 20:32:21 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -17,7 +17,7 @@ export default function TouchMeCard() {
                     <div className='h-full'>
                         <h2 className='font-[1000] text-3xl'>公众号</h2>
                         <h3 className='pt-2'>快人一步获取最新文章</h3>
-                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://jsd.cdn.zzko.cn/gh/88lin/PicX@master/gzh_cover.png) center center no-repeat' }}></div>
+                        <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://520.txsb.gq/gzh/index.html) center center no-repeat' }}></div>
                     </div>}
                 backContent={<Link href={CONFIG.SOCIAL_CARD_URL}>
                     <div className='font-[1000] text-xl h-full'>
