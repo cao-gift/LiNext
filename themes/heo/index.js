ccbf0e41f45604bc4493a95d075555daeb59e16d
故人 <111191349+88lin@users.noreply.github.com>
2024-03-03 10:59:02 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -289,7 +289,7 @@ const LayoutMemos = (props) => {
                     <i className="fas fa-comment mr-1" />
                     {locale.COMMON.COMMENTS}
                   </div>
-                  <Comment frontMatter={post} className="" />
+                  <Comment frontMatter={memoPageInfo} className="" />
                 </div>
               </div>}
           </div>
