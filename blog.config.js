1d49bb8840a14c480529064bc604211084e76350
故人 <111191349+88lin@users.noreply.github.com>
2024-03-23 23:47:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -52,7 +52,7 @@ const BLOG = {
   FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-medium',
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
-    'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+    'https://cdn.staticfile.net/lxgw-wenkai-webfont/1.7.0/style.css',
     'https://fonts.googleapis.com/css?family=Bitter&display=swap', // Bitter 用作英文数字的字体
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
@@ -63,7 +63,6 @@ const BLOG = {
     '-apple-system',
     'BlinkMacSystemFont',
     '"Microsoft YaHei"',
-    '"LXGW WenKai"',
     '"Segoe UI Emoji"',
     '"Apple Color Emoji"',
     '"Segoe UI Symbol"',
@@ -73,6 +72,7 @@ const BLOG = {
     '"Helvetica Neue"',
     'Helvetica',
     '"Source Han Sans SC"',
+    '"LXGW WenKai"',
     'Arial'
   ],
   // 衬线字体 例如'"LXGW WenKai"'
