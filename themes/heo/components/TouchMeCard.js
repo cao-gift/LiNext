e183e9ceb9f8a54fec8c5b1c6a65e56792cc1891
故人 <111191349+88lin@users.noreply.github.com>
2024-03-30 22:41:44 +0800

    Update TouchMeCard.js

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -1,4 +1,3 @@
-
 import FlipCard from '@/components/FlipCard'
 import Link from 'next/link'
 import CONFIG from '../config'
@@ -16,11 +15,11 @@ export default function TouchMeCard() {
         <div className={'relative h-28 text-white flex flex-col'}>
 
             <FlipCard
-                className='cursor-pointer lg:p-6 p-4 border rounded-xl bg-gray-100 dark:bg-yellow-600 dark:border-gray-600'
+                className='cursor-pointer lg:p-6 p-4 border rounded-xl bg-[#4f65f0] dark:bg-yellow-600 dark:border-gray-600'
                 frontContent={
                     <div className='h-full'>
-                        <h2 className='font-[1000] text-3xl'>公众号</h2>
-                        <h3 className='pt-2'>快人一步获取最新文章</h3>
+                        <h2 className='font-[1000] text-3xl' style='font-size: 2.7rem'>公众号</h2>
+                        <h3 className='pt-2' style='font-size: 1.3rem'>快人一步获取最新文章</h3>
                         <div className='absolute left-0 top-0 w-full h-full' style={{ background: 'url(https://www.jsdelivr.ren/gh/88lin/PicX@master/gzh_cover.png) center center no-repeat' }}></div>
                     </div>}
                 backContent={<Link href={siteConfig('HEO_SOCIAL_CARD_URL', null, CONFIG)}>
