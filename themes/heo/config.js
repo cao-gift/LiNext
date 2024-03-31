4a6fe43fed65c067c36958d29bf636c1bad2c16a
故人 <111191349+88lin@users.noreply.github.com>
2024-03-31 21:49:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,7 +28,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   HERO_RECOMMEND_COVER: '', // 英雄区右侧图片
 
