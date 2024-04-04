900516c90ae68b417830d4c87270091e9547cfa7
故人 <111191349+88lin@users.noreply.github.com>
2024-04-04 23:33:25 +0800

    Update useAdjustStyle.js

--- hooks/useAdjustStyle.js
+++ hooks/useAdjustStyle.js
@@ -31,30 +31,6 @@ const useAdjustStyle = () => {
       };
     }
   }, []);
-
-  // 强行移除 react-notion-x 表格表头第一行样式
-  useEffect(() => {
-    for (let i = 0; i < document.styleSheets.length; i++) {
-      const styleSheet = document.styleSheets[i];
-      if (styleSheet.href && styleSheet.href.includes('http')) {
-        continue; // 忽略指定样式表，避免访问报错
-      }
-      try {
-        const rules = styleSheet.cssRules;
-        for (let j = 0; j < rules.length; j++) {
-          const rule = rules[j];
-          if (rule.selectorText === '.notion-simple-table tr:first-child td') {
-            styleSheet.deleteRule(j);
-            break;
-          }
-        }
-      } catch (error) {
-        console.error('Error accessing stylesheets:', error);
-      }
-    }
-    return () => {
-    };
-  }, []);
 };
 
 export default useAdjustStyle;
