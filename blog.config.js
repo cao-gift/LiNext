563fd6d7c48ba1fe93e2cad4c398f676c3d1a81a
故人 <111191349+88lin@users.noreply.github.com>
2024-04-10 15:57:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -382,7 +382,7 @@ const BLOG = {
 
   SEO_GOOGLE_SITE_VERIFICATION: process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
 
-  SEO_BAIDU_SITE_VERIFICATION: process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || 'codeva-5Gs7LGUeHJ', // 百度站长平台的站点验证
+  SEO_BAIDU_SITE_VERIFICATION: process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || 'codeva-bBW3o4leNz', // 百度站长平台的站点验证
 
   // 微软 Clarity 站点分析
   CLARITY_ID: process.env.NEXT_PUBLIC_CLARITY_ID || null, // 只需要复制Clarity脚本中的ID部分，ID是一个十位的英文数字组合
